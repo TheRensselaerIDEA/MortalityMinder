60b27a9161aad084f22fae39ea7df217121467ea
John Erickson <olyerickson@gmail.com>
2019-09-27 15:23:22 -0400

    Now loading binary version of dataframes from wd

--- beta.03/Source.R
+++ beta.03/Source.R
@@ -1,9 +1,13 @@
 setwd("init")
 source("Curl_fixer.R")
 source("Librarian.R")
-source("Loader_CHR2019.R") # Added write_rds 9/27/2019
-source("Loader_CDC.R")     # Added write_rds 9/27/2019
-source("Loader_GEO.R")     # Added write_rds 9/27/2019
+#source("Loader_CHR2019.R") # Added write_rds 9/27/2019
+read_rds("chr.data.2019.rds")
+read_rds("chr.namemap.2019.rds")
+#source("Loader_CDC.R")     # Added write_rds 9/27/2019
+read_rds("cdc.data.rds")
+#source("Loader_GEO.R")     # Added write_rds 9/27/2019
+read_rds("geo.namemap.rds")
 source("Analyzer_PCA.R")
 source("Analyzer_Kmeans.R")
 source("Analyzer_Correlation.R")
