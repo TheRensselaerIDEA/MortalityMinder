e8f1994934e695e010f81a9e91a313a4fc7107bb
Karan Bhanot <bhanotkaran22@gmail.com>
2019-11-01 11:05:04 -0400

    Fix geo.namemap

--- Source.R
+++ Source.R
@@ -1,7 +1,7 @@
 setwd("init")
 source("Curl_fixer.R")
 source("Librarian.R")
-
+library(withr)
 # Separated out function definitions 10/01/2019
 #source("Loader_CHR2019.R") # Added write_rds 9/27/2019
 source("CHR2019_Lib.R")     # Function definitions from Loader_CHR2019.R
@@ -13,8 +13,8 @@ chr.namemap.inv.2019 <- readRDS("chr.namemap.inv.2019.rds")
 # Separated out function definitions 10/01/2019
 #source("Loader_CDC.R")     # Added write_rds 9/27/2019
 source("CDC_Lib.R")         # Function definitions from Loader_CDC.R
-cdc.data <- readRDS("../data/CDC/cdc.data.imputed.Rds")
-# cdc.data <- readRDS("cdc.data.dev.rds")
+# cdc.data <- readRDS("../data/CDC/cdc.data.imputed.Rds")
+cdc.data <- readRDS("cdc.data.dev.rds")
 # need impute non zero death_rate
 # cdc.data$death_rate[cdc.data$death_rate < 1] <- 1
 # Separated out function definitions 10/01/2019
