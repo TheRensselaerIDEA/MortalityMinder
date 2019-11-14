f0a4d7e382b2218f7ad44a143d9566d6acc9ca25
John Erickson <olyerickson@gmail.com>
2019-11-14 05:36:57 -0500

    added transparency script

---
+++ www/National_image/image_fixer.R
@@ -0,0 +1,103 @@
+# Uses imagicmagik via 'magick'
+library(magick)
+
+# Add transparency to "Despair" images
+frink <- image_read("Despair/1.png")
+frink_x <- image_transparent(frink, "white", fuzz = 0)
+image_write(frink_x, "Despair/1.png")
+
+frink <- image_read("Despair/2.png")
+frink_x <- image_transparent(frink, "white", fuzz = 0)
+image_write(frink_x, "Despair/2.png")
+
+frink <- image_read("Despair/3.png")
+frink_x <- image_transparent(frink, "white", fuzz = 0)
+image_write(frink_x, "Despair/3.png")
+
+frink <- image_read("Despair/4.png")
+frink_x <- image_transparent(frink, "white", fuzz = 0)
+image_write(frink_x, "Despair/4.png")
+
+frink <- image_read("Despair/5.png")
+frink_x <- image_transparent(frink, "white", fuzz = 0)
+image_write(frink_x, "Despair/5.png")
+
+frink <- image_read("Despair/6.png")
+frink_x <- image_transparent(frink, "white", fuzz = 0)
+image_write(frink_x, "Despair/6.png")
+
+# Add transparency to "Assault" images
+frink <- image_read("Assault/1.png")
+frink_x <- image_transparent(frink, "white", fuzz = 0)
+image_write(frink_x, "Assault/1.png")
+
+frink <- image_read("Assault/2.png")
+frink_x <- image_transparent(frink, "white", fuzz = 0)
+image_write(frink_x, "Assault/2.png")
+
+frink <- image_read("Assault/3.png")
+frink_x <- image_transparent(frink, "white", fuzz = 0)
+image_write(frink_x, "Assault/3.png")
+
+frink <- image_read("Assault/4.png")
+frink_x <- image_transparent(frink, "white", fuzz = 0)
+image_write(frink_x, "Assault/4.png")
+
+frink <- image_read("Assault/5.png")
+frink_x <- image_transparent(frink, "white", fuzz = 0)
+image_write(frink_x, "Assault/5.png")
+
+frink <- image_read("Assault/6.png")
+frink_x <- image_transparent(frink, "white", fuzz = 0)
+image_write(frink_x, "Assault/6.png")
+
+# Add transparency to "Cardio" images
+frink <- image_read("Cardio/1.png")
+frink_x <- image_transparent(frink, "white", fuzz = 0)
+image_write(frink_x, "Cardio/1.png")
+
+frink <- image_read("Cardio/2.png")
+frink_x <- image_transparent(frink, "white", fuzz = 0)
+image_write(frink_x, "Cardio/2.png")
+
+frink <- image_read("Cardio/3.png")
+frink_x <- image_transparent(frink, "white", fuzz = 0)
+image_write(frink_x, "Cardio/3.png")
+
+frink <- image_read("Cardio/4.png")
+frink_x <- image_transparent(frink, "white", fuzz = 0)
+image_write(frink_x, "Cardio/4.png")
+
+frink <- image_read("Cardio/5.png")
+frink_x <- image_transparent(frink, "white", fuzz = 0)
+image_write(frink_x, "Cardio/5.png")
+
+frink <- image_read("Cardio/6.png")
+frink_x <- image_transparent(frink, "white", fuzz = 0)
+image_write(frink_x, "Cardio/6.png")
+
+# Add transparency to "Cancer" images
+frink <- image_read("Cancer/1.png")
+frink_x <- image_transparent(frink, "white", fuzz = 0)
+image_write(frink_x, "Cancer/1.png")
+
+frink <- image_read("Cancer/2.png")
+frink_x <- image_transparent(frink, "white", fuzz = 0)
+image_write(frink_x, "Cancer/2.png")
+
+frink <- image_read("Cancer/3.png")
+frink_x <- image_transparent(frink, "white", fuzz = 0)
+image_write(frink_x, "Cancer/3.png")
+
+frink <- image_read("Cancer/4.png")
+frink_x <- image_transparent(frink, "white", fuzz = 0)
+image_write(frink_x, "Cancer/4.png")
+
+frink <- image_read("Cancer/5.png")
+frink_x <- image_transparent(frink, "white", fuzz = 0)
+image_write(frink_x, "Cancer/5.png")
+
+frink <- image_read("Cancer/6.png")
+frink_x <- image_transparent(frink, "white", fuzz = 0)
+image_write(frink_x, "Cancer/6.png")
+
