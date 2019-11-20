7e3aba7316f4cc1aee4b5621f9426e2094201b87
Karan Bhanot <bhanotkaran22@gmail.com>
2019-11-20 12:25:35 -0500

    Add source text

--- app.R
+++ app.R
@@ -377,6 +377,7 @@ ui <- fluidPage(
               tags$p(htmlOutput("determinant_text")),
               tags$h5(htmlOutput("determinant_corr")),
               tags$h5(htmlOutput("determinant_dir")),
+              tags$h5(htmlOutput("determinant_original_source")),
               tags$p(uiOutput("determinant_link"))
                     ), # End of Column 3 top
             fluidRow(
@@ -818,7 +819,16 @@ server <- function(input, output, session) {
     return(as.character(
       SocialDeterminants[SocialDeterminants$Name == input$determinant_choice,]$"URL"))
   })
+  
+  determinant.source <- reactive({
+    return(as.character(
+      SocialDeterminants[SocialDeterminants$Name == input$determinant_choice,]$"Source"))
+  })
 
+  determinant.source_url <- reactive({
+    return(as.character(
+      SocialDeterminants[SocialDeterminants$Name == input$determinant_choice,]$"Source_url"))
+  })
   
   # ----------------------------------------------------------------------
   # Functions for data download
@@ -1314,6 +1324,18 @@ server <- function(input, output, session) {
       tags$a(
         "Click here for more information",
         href = determinant.url(),
+        target="_blank"
+      )
+    )
+    )
+  })
+  
+  output$determinant_original_source <- renderUI({
+    tagList(tags$h4(
+      "Source: ",
+      tags$a(
+        determinant.source(),
+        href = determinant.source_url(),
         target="_blank"
       )
     )
