573d23970538f857ecaf9b9004588c15d282f8e1
Karan Bhanot <bhanotkaran22@gmail.com>
2019-11-26 12:59:34 -0500

    Generate geo plots for USA

---
+++ national_geomap_generator.R
@@ -0,0 +1,90 @@
+# Author: Karan Bhanot
+# Adapted from "national_map_generator.R and Theme.R
+# Create geo plot images for USA
+
+library(htmltools)
+library(webshot)
+
+deps <- list("topojson.min.js", 
+             htmlDependency(name = "d3-scale-chromatic",
+                            version = "1.3.3",
+                            src = list(href = "https://d3js.org/"),
+                            script = "d3-scale-chromatic.v1.min.js")
+)
+
+ui <- fluidPage(
+  # include css
+  tags$head(includeCSS("~/data/AHRQ_Challenge/App_develop/custom_no_scroll.css")),
+  tags$head(includeCSS("~/data/AHRQ_Challenge/Code_by_ziyi/git2/MortalityMinder/www/geoattr.css")),
+  tags$head(
+    tags$script(src="jquery-3.4.1.min.js"),
+    tags$script("$.noConflict(true);")),
+  
+  tags$title("D3 NY"),
+  tags$div(
+    class = "title",
+    tags$b("National Death of Despair")
+  ),
+  tags$div(
+    class = "explore_but",
+    tags$button(
+      id = "play",
+      "See changes"
+    ),
+    tags$span(
+      id = "clock",
+      "2000-2002"
+    ),
+    tags$button(
+      class = "btn",
+      "Explore more",
+      onclick="window.location.href=' https://shengjin-li.shinyapps.io/beta/'"
+    ) 
+  ),
+  d3Output("d3", width = '100%', height = '100%'),
+  tags$body(),
+  tags$script(src = "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js"),
+  tags$script(src = "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js"),
+  tags$script(src = "jquery-ui.min.js")
+)
+
+data_to_json <- function(data) {
+  jsonlite::toJSON(data, dataframe = "rows", auto_unbox = FALSE, rownames = TRUE)
+}
+
+server <- function(input, output) {
+  output$d3 <- renderD3({
+    data_geo <- jsonlite::read_json("www/all-counties.json")
+    
+    causes_of_death <- c("Despair", "Assault", "Cancer", "Cardio", "All Cause")
+    
+    for(cause in causes_of_death){
+      if (cause == "Cardio") {
+        cause = paste(cause, "vascular", sep = "")
+      }
+      
+      mort.cluster.raw <- cluster.counties(cdc.mort.mat(cdc.data, "US", cause), cluster.method="kmeans", cluster.num=6)
+      mort.cluster <- order.county.clusters(mort.cluster.raw,
+                                            get.cluster.order.map(get.cluster.deathrate.during.time(mort.cluster.raw,
+                                                                                                    cdc.data,
+                                                                                                    death.cause=cause
+                                            ), time.period = "2015-2017"))
+
+      dataset <- geo.map.fetch("US", mort.cluster) %>% dplyr::select(county_fips, cluster) %>% dplyr::arrange(county_fips)
+
+      map <- r2d3(data = list(data_geo,data_to_json(dataset),data_to_json(cause)),
+                  d3_version = 3,
+                  dependencies = "topojson.min.js",
+                  css = "www/geoattr.css",
+                  script = "www/d3_geo_animation.js")
+      
+      address <- paste0("www/National_geo_image/",cause)
+      address <- paste0(address,".png")
+      
+      save_d3_png(map, address, width = 992,
+                  height = 544, delay = 1, zoom = 1)
+    }
+    print("finished generating map")
+  })
+}
+shinyApp(ui, server)
---
+++ www/d3_geo_animation.js
@@ -0,0 +1,146 @@
+//Author: Ziyi
+
+var width = 400,
+height = 600;
+var root = svg
+.attr("width", "100%")
+.attr("height", "100%")
+.append("g");
+
+function sequenceMap(index,tooltip,cause) {
+  var clusters = [1,2,3,4,5,6]
+  
+  var color = d3.scale.threshold()
+  .domain(clusters)
+  .range(["#fed976", "#fd8d3c", "#fc4e2a", "#e31a1c", "#bd0026", "#800026"]);
+  svg.selectAll('.state').transition()  //select all the state element and prepare for a transition to new values
+  .duration(750)  // give it a smooth time period for the transition
+  .attr('fill', function(d) {
+    return color(d.properties.cluster);  // the end color value
+  }
+  )
+  svg.selectAll('.state')
+  .on("mouseover", function(d) {    
+    tooltip.transition()    
+    .duration(200)    
+    .style("opacity", .9);    
+    tooltip.html(d.properties.name + "\n" + d.properties.death_rate[index])  
+    .style("left", (d3.event.pageX) + "px")   
+    .style("top", (d3.event.pageY - 28) + "px");  
+  })          
+  .on("mouseout", function(d) {   
+    tooltip.transition()    
+    .duration(500)    
+    .style("opacity", 0); 
+  });
+}
+
+d3.select("body").append("div");
+
+r2d3.onRender(function(data, svg, width, height, options) {
+  var us = data[0];
+  var stat = data[1];
+  var cause = data[2];
+  var clusters = [1,2,3,4,5,6]
+  var death_rate = d3.map();
+  var color = d3.scale.threshold()
+  .domain(clusters)
+  .range(["#fed976", "#fd8d3c", "#fc4e2a", "#e31a1c", "#bd0026", "#800026"]);
+  
+  
+  var tooltip = d3.select("body").append("div") 
+  .attr("class", "tooltip")       
+  .style("opacity", 0);
+  
+  var geo = topojson.feature(us, us.objects.collection);
+  //only newyork state data
+  var geo2 = geo.features;
+  
+  path = d3.geo.path();
+  for(var i = 0; i < stat.length; i++){
+    var dataFips = stat[i].county_fips;
+    // Grab data value 
+    var dataValue = [stat[i]["cluster"]];
+    for(var j = 0; j < geo2.length; j++){
+      var jsonFips = geo2[j].properties.fips
+      if (dataFips == jsonFips) {
+        geo2[j].properties.cluster = dataValue;
+        break;
+      }
+    }
+  }    
+  
+  //map      
+  root.selectAll(".state")
+  .data(geo2)
+  .enter().append("path")
+  .attr("class", "state")
+  .attr("d", path)
+  .attr("fill",function(d){
+    return color(d.properties.cluster);
+  })
+  .on("mouseover", function(d) {    //the hovering effect
+    tooltip.transition()    
+    .duration(200)    
+    .style("opacity", .9);    
+    tooltip.html(d.properties.name + "\n" + d.properties.cluster)  
+    .style("left", (d3.event.pageX) + "px")   
+    .style("top", (d3.event.pageY - 28) + "px");  
+  })          
+  .on("mouseout", function(d) {   
+    tooltip.transition()    
+    .duration(500)    
+    .style("opacity", 0); 
+  });
+  
+  
+  
+  var rect = svg
+  .attr("width", "100%")
+  .attr("height", "100%")
+  //color scale        
+  rect.selectAll("rect")
+  .data(clusters)
+  .enter()
+  .append("rect")
+  .attr({
+    width: 24,
+    height: 5,
+    y: 520,
+    x: function (d, i) {
+      return 550 + 25 * i;
+    },
+    fill: color
+  });
+  
+  svg.selectAll("text")
+  .data(clusters)
+  .enter()
+  .append("text")
+  .attr({
+    y:520,
+    x:function(d,i){
+      return 550 + 25 * i;
+    }})
+  .text(function(d){
+    return d;
+  })
+  ;
+  
+  var index = 0;        
+  var timer;
+  var playing = false;
+  var year;
+  var year2;
+  var ten_year;
+  var ten_year2;
+  setInterval(function(){   // set a JS interval
+    if(index < 5) {  
+      index += 1;  // increment the current attribute counter
+    } else {
+      index = 0;  // or reset it to zero
+    }
+    sequenceMap(index,tooltip,cause);
+    
+  }, 2000);
+});
