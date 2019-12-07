f13c8c491487445a165060eb0137fb10796dda4c
Karan Bhanot <bhanotkaran22@gmail.com>
2019-12-07 13:00:23 -0500

    Remove excess code from Source.R and make it more readable

--- Source.R
+++ Source.R
@@ -1,15 +1,12 @@
 setwd("init")
+
+library(withr)
+
 source("Curl_fixer.R")
 source("Librarian.R")
-library(withr)
-# Separated out function definitions 10/01/2019
-#source("Loader_CHR2019.R") # Added write_rds 9/27/2019
-source("CHR2019_Lib.R")     # Function definitions from Loader_CHR2019.R
+source("CHR2019_Lib.R") # Function definitions from Loader_CHR2019.R
 
-# Read in new Social Determinants definitions
-#SocialDeterminants <- read_csv("../init/SocialDeterminants_updated.csv")
 SocialDeterminants <- read_csv("../init/SocialDeterminants.csv")
-
 final.determinants <- SocialDeterminants[SocialDeterminants$Keep == 1,]["Code"]
 final.determinants <- append(final.determinants$Code, "county_fips", after = 0)
 
@@ -30,20 +27,15 @@ names(chr.namemap.inv.2019)[1] <- "code"
 
 # Load state and national mortality rate data
 state_natl_death_rates <- readRDS("../data/CDC/state_natl_death_rates.Rds")
-  
-# Separated out function definitions 10/01/2019
-#source("Loader_CDC.R")     # Added write_rds 9/27/2019
-source("CDC_Lib.R")         # Function definitions from Loader_CDC.R
+
+source("CDC_Lib.R") # Function definitions from Loader_CDC.R
 cdc.unimputed.data <- readRDS("../data/cdc.data.rds")
 cdc.data <- readRDS("../data/CDC/cdc.data.imputed.Rds")
 cdc.suppress <- readRDS("../data/CDC/cdc.suppress.Rds")
-# cdc.data <- readRDS("cdc.data.dev.rds")
-# need impute non zero death_rate
-# cdc.data$death_rate[cdc.data$death_rate < 1] <- 1
-# Separated out function definitions 10/01/2019
-#source("Loader_GEO.R")     # Added write_rds 9/27/2019
-source("GEO_Lib.R")         # Function definitions from Loader_GEO.R
+
+source("GEO_Lib.R") # Function definitions from Loader_GEO.R
 geo.namemap <- readRDS("geo.namemap.rds")
+
 source("Analyzer_PCA.R")
 source("Clustering_Lib.R")
 source("Analyzer_Correlation.R")
