79b97b09c2b55f31a107ca5c3c06ba55925713c0
John Erickson <olyerickson@gmail.com>
2019-12-12 20:59:39 -0500

    edited headers

--- app.R
+++ app.R
@@ -60,10 +60,6 @@ ui <- fluidPage(
     tags$div(
       class = "title",
       tags$h1(
-        # tags$img(
-        #   src="RPIlogo_black.png"
-        #   # height="30px"
-        #   ),
         "MortalityMinder")
       ),
     tags$div(
@@ -917,11 +913,11 @@ server <- function(input, output, session) {
   output$page1_main_header <- renderUI({
     if (input$state_choice == "United States") {
       tags$h3(
-        paste0("What are the trends in midlife mortality rates for ", names(which(cause.list == input$death_cause)), " across the United States?")
+        paste0("Nationwide View: What are the trends in midlife mortality rates for ", names(which(cause.list == input$death_cause)), " across the United States?")
       )
     } else {
       tags$h3(
-        paste0("What are the trends in midlife mortality rates for ", names(which(cause.list == input$death_cause)), " across the United States and in ", 
+        paste0("Nationwide View: What are the trends in midlife mortality rates for ", names(which(cause.list == input$death_cause)), " across the United States and in ", 
                names(which(state.list == input$state_choice)), "?")
       )
     }
@@ -935,7 +931,7 @@ server <- function(input, output, session) {
       location_str = names(which(state.list == input$state_choice))
     }
     tags$h3(
-      paste0("How do midlife mortality rates for ", names(which(cause.list == input$death_cause)), " vary by county across ", location_str, " and why?")
+      paste0("State View: How do midlife mortality rates for ", names(which(cause.list == input$death_cause)), " vary by county across ", location_str, " and why?")
     )
   })
   
@@ -946,7 +942,7 @@ server <- function(input, output, session) {
       location_str = names(which(state.list == input$state_choice))
     }
     tags$h3(
-      paste0("How are county-level social and economic factors associated with midlife mortality rates for ", names(which(cause.list == input$death_cause)), " in ", location_str,"?")
+      paste0("Factor View: How are county-level social and economic factors associated with midlife mortality rates for ", names(which(cause.list == input$death_cause)), " in ", location_str,"?")
     )
   })
   
@@ -2162,11 +2158,8 @@ server <- function(input, output, session) {
     # We reference state.list, cause.list and cause.definitions defined above
     
     tagList(
-      # HTML(
-      # paste0("<h3><b>State View:</b> ", names(which(cause.list == input$death_cause)), " in the State of ", names(which(state.list == input$state_choice)), " and their Associated Disparities</h3>")
-      # ),
-      tags$h4(paste0(names(which(cause.definitions == input$death_cause)))),
-      HTML("<h5>In this analysis, counties are categorized into <b>risk groups</b> according to risk based on their midlife mortality rate trends.</h5>"),
+      tags$h5(paste0(names(which(cause.definitions == input$death_cause)))),
+      HTML("<h5>In this analysis, counties that share similar midlife mortality rate trends are categorized into <b>risk groups</b>.</h5>"),
       HTML("<h5>The <b>upper map</b> to the right shows the <b>midlife mortality rates</b> of the counties over time. The <b>lower map</b> on the left shows the <b>risk group</b> of each county. The <b>line graph</b> on the lower left compares the average mortality rates per year for each risk group  with the national mean (blue)."),
       HTML("<h5><b>Darker colors</b> indicate increased midlife mortality risk. <b>Hover</b> to see information and definitions. <b>Click on maps</b> to see county names and mortality rates. <b>Zoom maps</b> with buttons or mouse."), 
       HTML("<h5><span style='color:white'>Click <b>BACK</b></span> <b><span style='color:#00bfc4'>&lt;&lt;</span></b> <span style='color:white'>and <b>NEXT</b> </span>
@@ -2200,7 +2193,7 @@ server <- function(input, output, session) {
     
     tagList(
       tags$h4(
-        title ="Midlife mortality trates are obtained from the Detailed Mortality Online Mortality Database at https://wonder.cdc.gov/.  Separate crude death rates are queried  for adults 25 to 64 at the county, state, and nationwide levels for each cause of death.  Rates are not age adjusted. Unreliable or missing rates are imputed. See Project Overview for details.",
+        title ="Midlife mortality rates are obtained from the CDC WONDER Detailed Mortality Online Mortality Database.  Separate crude death rates are queried  for adults 25 to 64 at the county, state, and nationwide levels for each cause of death.  Rates are not age adjusted. Unreliable or missing rates are imputed. See Project Overview for details.",
         paste0("Midlife Mortality Rate: Deaths per 100,000 for adults ages 25-64 due to ",
                names(which(cause.list == input$death_cause)), 
                " for three year periods for counties (left) and state and nation (right)."
@@ -2354,7 +2347,8 @@ server <- function(input, output, session) {
     
     tagList(
       tags$h1(
-        paste0("Nationwide View: ",names(which(cause.list == input$death_cause)), " Midlife Mortality Rates Over Time")
+        # paste0("Nationwide View: ",names(which(cause.list == input$death_cause)), " Midlife Mortality Rates Over Time")
+        paste0(names(which(cause.list == input$death_cause)), " Midlife Mortality Rates Over Time")
       )
     )
   })
@@ -2449,8 +2443,8 @@ server <- function(input, output, session) {
     tagList(
       tags$h3(
         title="This plot represents the distribution of midlife mortality rates (ages 25-64) for the selected state.",
-        paste0("State View: ",names(which(cause.list == input$death_cause)), " Midlife Mortality Rates for ", names(which(state.list == input$state_choice))," for ",input$year_selector)
-        # , icon("info-circle")
+        # paste0("State View: ",names(which(cause.list == input$death_cause)), " Midlife Mortality Rates for ", names(which(state.list == input$state_choice))," for ",input$year_selector)
+        paste0(names(which(cause.list == input$death_cause)), " Midlife Mortality Rates for ", names(which(state.list == input$state_choice))," for ",input$year_selector)
       ),
       tags$h6("The geographic distribution of midlife mortality rates (ages 25-64) for ",names(which(state.list == input$state_choice))),
       NULL
@@ -2568,8 +2562,8 @@ server <- function(input, output, session) {
       tagList(
         tags$h3(
           title="Boxplot shows the distribution of the factor within each cluster. The middle line is the median. For destructive factors, boxes will shift up for higher risk groups. For protective factors, boxes will shift down for high risk groups.",
-          paste0("Factor View: ",input$determinant_choice, " and Risk Group Relationship for ", location_str)
-          # , icon("info-circle")
+          # paste0("Factor View: ",input$determinant_choice, " and Risk Group Relationship for ", location_str)
+          paste0(input$determinant_choice, " and Risk Group Relationship for ", location_str)
         ),
         HTML("<h5>Distribution within each cluster. The middle line is the median. For <span style='color:#f8766d'>Destructive</span> (<span style='color:#00bfc4'>Protective</span>) factors, boxes will shift <span style='color:#f8766d'>up</span> (<span style='color:#00bfc4'>down</span>) for higher risk groups."),
         NULL
@@ -2579,8 +2573,8 @@ server <- function(input, output, session) {
     tagList(
       tags$h3(
         title="Boxplot shows the distribution of the factor within each cluster. The middle line is the median. For destructive factors, boxes will shift up for higher risk groups. For protective factors, boxes will shift down for high risk groups.",
-        paste0("Factor View: ",input$determinant_choice, " and Risk Group Relationship for ", names(which(state.list == input$state_choice)))
-        # ,icon("info-circle")
+        # paste0("Factor View: ",input$determinant_choice, " and Risk Group Relationship for ", names(which(state.list == input$state_choice)))
+        paste0(input$determinant_choice, " and Risk Group Relationship for ", names(which(state.list == input$state_choice)))
       ),
       HTML("<h5>Distribution within each cluster. The middle line is the median. For <span style='color:#f8766d'>Destructive</span> (<span style='color:#00bfc4'>Protective</span>) factors, boxes will shift <span style='color:#f8766d'>up</span> (<span style='color:#00bfc4'>down</span>) for higher risk groups."),
       NULL
